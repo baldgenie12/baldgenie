5c20e3b0f00442e6ead83fed2fcc2c52bd855f44
bhola <bhola2266@gmail.com>
2022-04-23 13:33:33 +0530

    commit

--- pages/partner_with_us.js
+++ pages/partner_with_us.js
@@ -5,6 +5,50 @@ import {
 
 const MerchantSignUP = () => {
 
+    const [loading, setloading] = useState(false)
+
+
+
+
+
+    //Drop down Menu items
+
+    const serviceRadiusMenu = [
+        '1', '2', '3', '4', '5',
+        '6', '7', '8', '9', '10',
+        '11', '12', '13', '14', '15',
+        '16', '17', '18', '19', '20',
+        '21', '22', '23', '24', '25'
+    ]
+
+
+    const bussinessHoursMenu = [
+        'All days', 'Monday - Friday', 'Saturday - Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
+    ]
+
+    const timezoneMenu = ['Eastern Time', 'Central Time', 'Mountain Time', 'Pacific Time']
+
+    const yearsinBussinessMenu = [
+        '0', '1', '2', '3', '4', '5', '6',
+        '7', '8', '9', '10', '11', '12', '13',
+        '14', '15', '16', '17', '18', '19', '20',
+        '21', '22', '23', '24', '25', '26', '27',
+        '28', '29', '30', '31', '32', '33', '34',
+        '35', '36', '37', '38', '39', '40', '41',
+        '42', '43', '44', '45', '46', '47', '48',
+        '49', '50'
+    ]
+
+    const monthsinBussinessMenu = ['1', '2', '3', '4', '5', '6',
+        '7', '8', '9', '10', '11']
+
+    const employeeStrengthMenu = ['Solo', '2-5', '6-10', '11-20', '20-50', '50+ ']
+
+    for (let index = 0; index < 51; index++) {
+        monthsinBussinessMenu.push(index.toString())
+    }
+
+
 
     const [nameTitle, setnameTitle] = useState('Mr.')
     const [firstName, setfirstName] = useState('')
@@ -40,14 +84,88 @@ const MerchantSignUP = () => {
     const [serviceRadiusFrom, setserviceRadiusFrom] = useState('')
     const [serviceRadiusTo, setserviceRadiusTo] = useState('')
     const [zipcodesCovered, setzipcodesCovered] = useState('')
-
     const [website, setwebsite] = useState('')
     const [worknumber, setworknumber] = useState('')
 
+    //Timing
+
+    const [workingdays, setworkingdays] = useState('')
+    const [workingHours, setworkingHours] = useState('')
+    const [workingMinutes, setworkingMinutes] = useState('')
+
+    const [yearsinbusuiness, setyearsinbusuiness] = useState('')
+    const [monthsinbusiness, setmonthsinbusiness] = useState('')
+    const [employees, setemployees] = useState('')
 
 
 
+    const getLocationHnadler = () => {
 
+        navigator.geolocation.getCurrentPosition(position => {
+            fetchData(position.coords.latitude, position.coords.longitude)
+        }, showError)
+
+
+        function showError(error) {
+            switch (error.code) {
+                case error.PERMISSION_DENIED:
+                    setlocationErrorMsg("User denied the request for Geolocation.")
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    setlocationErrorMsg("Location information is unavailable.")
+
+                    break;
+                case error.TIMEOUT:
+                    setlocationErrorMsg("The request to get user location timed out.")
+
+                    break;
+                case error.UNKNOWN_ERROR:
+                    setlocationErrorMsg("An unknown error occurred.")
+
+                    break;
+                default:
+                    setlocationErrorMsg("An unknown error occurred.")
+
+
+            }
+        }
+        async function fetchData(latitude, longitude) {
+
+            if (location) {
+                return
+            }
+
+
+
+            if (!location) {
+                setloading(true)
+
+                var location = {}
+                try {
+                    const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&sensor=true&key=AIzaSyBq4_UrU1zgmlP2pmINbfbpu-O7MfY7F1c`)
+                    // const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=40.20197127507498,-100.66331257139588&sensor=true&key=AIzaSyBq4_UrU1zgmlP2pmINbfbpu-O7MfY7F1c`)
+                    location = await response.json()
+                    console.log(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&sensor=true&key=AIzaSyBq4_UrU1zgmlP2pmINbfbpu-O7MfY7F1c`);
+                    
+
+                    const part = location.plus_code.compound_code.trim()
+                    const index = part.indexOf(" ")
+                    const part2 = part.substring(index, part.length)
+
+                    setlocation(`${part2}`)
+                    setloading(false)
+
+                } catch (error) {
+                    locationErrorMsg ? alert(locationErrorMsg) : alert(error)
+                    setloading(false)
+
+                }
+                localStorage.setItem("location", JSON.stringify(location))
+            }
+
+
+        }
+    }
 
 
     return (
@@ -80,7 +198,7 @@ const MerchantSignUP = () => {
             <div className='flex items-center justify-start my-4 m-2 '>
                 <span className="dot"></span>
                 <h1 className='w-[230px] mr-3'>*Primary Contact Email</h1>
-                <input className="placeholder-gray-600  w-[250px]  p-1 text-sm text-center  rounded-lg   outline-none border-[0.5px]  border-gray-600 " onChange={e => setemail(e.target.value)} type="text" required placeholder="Enter Email" />
+                <input className="placeholder-gray-600  w-[250px]  p-1 text-sm text-center  rounded-lg   outline-none border-[0.5px]  border-gray-600 " onChange={e => setemail(e.target.value)} type="email" required placeholder="Enter Email" />
                 <p className='ml-2 text-sm text-gray-600'>(for us to contact you and will not be made public)</p>
             </div>
 
@@ -120,7 +238,7 @@ const MerchantSignUP = () => {
             <div className='flex items-center justify-start my-4 m-2 '>
                 <span className="dot"></span>
                 <h1 className='w-[230px] mr-3'>*Alternate Contact Email</h1>
-                <input className="placeholder-gray-600  w-[250px]  p-1 text-sm text-center  rounded-lg   outline-none border-[0.5px]  border-gray-600 " onChange={e => set_alternate_email(e.target.value)} type="text" required placeholder="Enter Email" />
+                <input className="placeholder-gray-600  w-[250px]  p-1 text-sm text-center  rounded-lg   outline-none border-[0.5px]  border-gray-600 " onChange={e => set_alternate_email(e.target.value)} type="email" required placeholder="Enter Email" />
                 <p className='ml-2 text-sm text-gray-600'>(for us to contact you and will not be made public)</p>
             </div>
 
@@ -157,7 +275,7 @@ const MerchantSignUP = () => {
             <div className='flex items-center justify-start my-4 m-2 '>
                 <span className="dot"></span>
                 <h1 className='w-[230px] mr-3 '>*Geo Location</h1>
-                <button className='hover:bg-gray-200 flex items-center justify-around pl-6 border-[0.5px]  border-black rounded-lg p-1 '>
+                <button onClick={getLocationHnadler} className='hover:bg-gray-200 flex items-center justify-around pl-6 border-[0.5px]  border-black rounded-lg p-1 '>
                     <LocationMarkerIcon className='h-6 text-red-600' />
                     <h1 className="placeholder-gray-600 w-[200px] cursor-pointer  text-sm text-center    outline-none "  >{location}</h1>
                 </button>
@@ -167,7 +285,12 @@ const MerchantSignUP = () => {
                 <span className="dot"></span>
                 <h1 className='w-[230px] mr-3'>*Service Radius</h1>
                 <select className='p-1 px-2 text-sm w-16 rounded-lg   outline-none border-[0.5px]  border-gray-600 ' value={serviceRadiusFrom} onChange={e => { setserviceRadiusFrom(e.target.value) }} >
-                    <option className='font-semibold py-4'>01</option>
+                    {serviceRadiusMenu.map(item => {
+                        return (
+
+                            <option key={item} className='font-semibold py-4'>{item}</option>
+                        )
+                    })}
 
                 </select>
                 <h2 className='ml-2'>Miles</h2>
@@ -180,7 +303,7 @@ const MerchantSignUP = () => {
             <div className='flex items-center justify-start my-4 m-2 '>
                 <span className="dot"></span>
                 <h1 className='w-[230px] mr-3'>*Zip Codes Covered</h1>
-                <input className="placeholder-gray-600 w-[250px]  p-1 text-sm text-center  rounded-lg   outline-none border-[0.5px]  border-gray-600 " onChange={e => setzipcodesCovered(e.target.value)} type="text" required placeholder="" />
+                <input className="placeholder-gray-600 w-[250px]  p-1 text-sm text-center  rounded-lg   outline-none border-[0.5px]  border-gray-600 " onChange={e => setzipcodesCovered(e.target.value)} type="number" required placeholder="" />
             </div>
 
 
@@ -196,19 +319,47 @@ const MerchantSignUP = () => {
             </div>
 
 
-            <div className='flex items-center justify-start my-4 m-2'>
+            <div className='flex items-start justify-start my-4 m-2'>
                 <span className="dot"></span>
                 <h1 className='w-[230px] mr-3'>*Business Hours</h1>
-                <select className='p-1 px-2 text-sm w-22 rounded-lg   outline-none border-[0.5px]  border-gray-600 ' value={serviceRadiusFrom} onChange={e => { setserviceRadiusFrom(e.target.value) }} >
-                    <option className='font-semibold py-4'>All Days</option>
-                </select>
-                <select className='ml-6 p-1 px-2 text-sm w-16 rounded-lg   outline-none border-[0.5px]  border-gray-600 ' value={serviceRadiusTo} onChange={e => { setserviceRadiusTo(e.target.value) }} >
-                    <option className='font-semibold py-4'>00</option>
-                </select>
-                <span className='ml-2'>-</span>
-                <select className='ml-2 p-1 px-2 text-sm w-16 rounded-lg   outline-none border-[0.5px]  border-gray-600 ' value={serviceRadiusTo} onChange={e => { setserviceRadiusTo(e.target.value) }} >
-                    <option className='font-semibold py-4'>12</option>
-                </select>
+                <div className='flex flex-col space-y-4'>
+                    <div>
+
+                        <select className='p-1 px-2 text-sm w-22 rounded-lg   outline-none border-[0.5px]  border-gray-600 ' value={workingdays} onChange={e => { setworkingdays(e.target.value) }} >
+                            {bussinessHoursMenu.map(item => {
+                                return (
+
+                                    <option key={item} className='font-semibold py-4'>{item}</option>
+                                )
+                            })}
+                        </select>
+                        <input className="placeholder-gray-600 w-[70px] ml-2  p-1 text-sm text-center  rounded-lg   outline-none border-[0.5px]  border-gray-600 " onChange={e => setworkingHours(e.target.value)} type="number" required placeholder="00" />
+
+
+                        <span className='ml-2'>-</span>
+                        <input className="placeholder-gray-600 w-[70px] ml-2  p-1 text-sm text-center  rounded-lg   outline-none border-[0.5px]  border-gray-600 " onChange={e => setworkingMinutes(e.target.value)} type="number" required placeholder="12" />
+
+                    </div>
+                    <div>
+
+                        <select className='p-1 px-2 text-sm w-22 rounded-lg   outline-none border-[0.5px]  border-gray-600 ' value={serviceRadiusFrom} onChange={e => { setserviceRadiusFrom(e.target.value) }} >
+                            {bussinessHoursMenu.map(item => {
+                                return (
+
+                                    <option key={item} className='font-semibold py-4'>{item}</option>
+                                )
+                            })}
+                        </select>
+                        <input className="placeholder-gray-600 w-[70px] ml-2  p-1 text-sm text-center  rounded-lg   outline-none border-[0.5px]  border-gray-600 " onChange={e => setwebsite(e.target.value)} type="number" required placeholder="00" />
+
+
+                        <span className='ml-2'>-</span>
+                        <input className="placeholder-gray-600 w-[70px] ml-2  p-1 text-sm text-center  rounded-lg   outline-none border-[0.5px]  border-gray-600 " onChange={e => setwebsite(e.target.value)} type="number" required placeholder="12" />
+
+                    </div>
+
+                    <button className='text-blue-500 font-bold text-left w-fit '>ADD</button>
+                </div>
             </div>
 
 
@@ -216,7 +367,12 @@ const MerchantSignUP = () => {
                 <span className="dot"></span>
                 <h1 className='w-[230px] mr-3'>Time Zone</h1>
                 <select className='p-1 px-3 text-sm w-22 rounded-lg   outline-none border-[0.5px]  border-gray-600 ' value={serviceRadiusFrom} onChange={e => { setserviceRadiusFrom(e.target.value) }} >
-                    <option className='font-semibold py-4'>Eastern</option>
+                    {timezoneMenu.map(item => {
+                        return (
+
+                            <option key={item} className='font-semibold py-4'>{item}</option>
+                        )
+                    })}
                 </select>
 
             </div>
@@ -225,13 +381,22 @@ const MerchantSignUP = () => {
             <div className='flex items-center justify-start my-4 m-2'>
                 <span className="dot"></span>
                 <h1 className='w-[230px] mr-3'>Years in Bussiness</h1>
-                <select className='p-1 px-2 text-sm w-16 rounded-lg   outline-none border-[0.5px]  border-gray-600 ' value={serviceRadiusFrom} onChange={e => { setserviceRadiusFrom(e.target.value) }} >
-                    <option className='font-semibold py-4'>01</option>
+                <select className='p-1 px-2 text-sm w-16 rounded-lg   outline-none border-[0.5px]  border-gray-600 ' value={yearsinbusuiness} onChange={e => { setyearsinbusuiness(e.target.value) }} >
+                    {yearsinBussinessMenu.map(item => {
+                        return (
 
+                            <option key={item} className='font-semibold py-4'>{item}</option>
+                        )
+                    })}
                 </select>
                 <h2 className='ml-2'>Years</h2>
-                <select className='ml-6 p-1 text-sm w-16 rounded-lg   outline-none border-[0.5px]  border-gray-600 ' value={serviceRadiusTo} onChange={e => { setserviceRadiusTo(e.target.value) }} >
-                    <option className='font-semibold py-4'>25+</option>
+                <select className='ml-6 p-1 text-sm w-16 rounded-lg   outline-none border-[0.5px]  border-gray-600 ' value={monthsinbusiness} onChange={e => { setmonthsinbusiness(e.target.value) }} >
+                    {monthsinBussinessMenu.map(item => {
+                        return (
+
+                            <option key={item} className='font-semibold py-4'>{item}</option>
+                        )
+                    })}
                 </select>
                 <h2 className='ml-2'>Months</h2>
             </div>
@@ -239,8 +404,13 @@ const MerchantSignUP = () => {
             <div className='flex items-center justify-start my-4 m-2'>
                 <span className="dot"></span>
                 <h1 className='w-[230px] mr-3'>Employee Strength</h1>
-                <select className='p-1 px-3 text-sm w-22 rounded-lg   outline-none border-[0.5px]  border-gray-600 ' value={serviceRadiusFrom} onChange={e => { setserviceRadiusFrom(e.target.value) }} >
-                    <option className='font-semibold py-4'>Solo</option>
+                <select className='p-1 px-3 text-sm w-22 rounded-lg   outline-none border-[0.5px]  border-gray-600 ' value={employees} onChange={e => { setemployees(e.target.value) }} >
+                    {employeeStrengthMenu.map(item => {
+                        return (
+
+                            <option key={item} className='font-semibold py-4'>{item}</option>
+                        )
+                    })}
                 </select>
 
             </div>
